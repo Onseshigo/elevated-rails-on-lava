7c237a06898a4ced2f38e8ad353f1bb9dc99240b
onseshigo <zr2.hch@gmail.com>
2025-05-16 02:20:10 +0300

    initial commit

---
+++ data-final-fixes.lua
@@ -0,0 +1,6 @@
+require("prototypes.technology")
+
+data.raw.tile["lava"].collision_mask.layers["rail"] = nil
+data.raw.tile["lava"].collision_mask.layers["rail_support"] = true
+data.raw.tile["lava-hot"].collision_mask.layers["rail"] = nil
+data.raw.tile["lava-hot"].collision_mask.layers["rail_support"] = true
---
+++ prototypes/technology.lua
@@ -0,0 +1,15 @@
+table.insert(
+  data.raw.technology["rail-support-foundations"].effects,
+  {
+    type = "nothing",
+    effect_description = {"modifier-description.rail-support-on-lava"},
+    icons =
+    {
+      {
+        icon = "__elevated_rails_on_lava__/graphics/icons/technology/effect/effect-rail-support-on-lava.png",
+        icon_size = 64,
+        icon_mipmaps = 1,
+      }
+    }
+  }
+)
