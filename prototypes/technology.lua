2b15ceb426b7369cfe511111f4f222bb058d90cf
onseshigo <zr2.hch@gmail.com>
2025-05-28 22:35:04 +0300

    removed mipmaps

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -4,14 +4,8 @@ if settings.startup["elevated-rails-on-lava-technology-effect-icon"].value then
     {
       type = "nothing",
       effect_description = {"modifier-description.rail-support-on-lava"},
-      icons =
-      {
-        {
-          icon = "__elevated-rails-on-lava__/graphics/icons/technology/effect/effect-rail-support-on-lava.png",
-          icon_size = 64,
-          icon_mipmaps = 1,
-        }
-      }
+      icon = "__elevated-rails-on-lava__/graphics/icons/technology/effect/effect-rail-support-on-lava.png",
+      icon_size = 64,
     }
   )
 end
